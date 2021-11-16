e3776988f8d9cbdfc9f10b319cb4f6b60e562455
Brian León Hoyos <brian.leon@uao.edu.co>
2021-11-15 23:16:50 -0500

    BD notas
    Imprimir las notas en la pagina extraidas desde la bd

--- src/routes/notesRutas.js
+++ src/routes/notesRutas.js
@@ -1,5 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const Note = require('../models/Note');
 
+router.get('/notes/add', function (req, res) {
+    res.render('notes/new-note');
+});
+
+router.post('/notes/new-note', async function (req, res) {
+    //console.log(req.body);
+    const { title, description } = req.body;
+    const errors = [];
+    if (!title) {
+        errors.push({ text: 'Por favor ingrese un titulo.' });
+    }
+    if (!description) {
+        errors.push({ text: 'Por favor ingrese una descripcion.' });
+    }
+    if (errors.length > 0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    }
+    else {
+        //res.send('ok');
+        const newNote = new Note({title, description});
+        await newNote.save();
+        //console.log(newNote);
+        res.redirect('/notes');
+    }
+});
+
+router.get('/notes', async function (req, res) {
+    const notes = await Note.find();
+    res.render('notes/all-notes', {notes});
+});
 
 module.exports = router;
